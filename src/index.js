639296be1678bc592915cb0a76f40dd3c579ccb8
Pranita <pfulsundar8@gmail.com>
2023-06-08 04:15:26 +0530

    chore: add backend, server files and packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { makeServer } from "./server";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Call make Server
+makeServer();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
