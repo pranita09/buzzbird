963f3a33953a296abd2fc96992635f7c262ef2bb
Pranita <pfulsundar8@gmail.com>
2023-06-08 20:56:14 +0530

    chore: install and configure tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState();
+  const getData = async () => {
+    try {
+      const response = await axios.get("/api/posts");
+      const {
+        status,
+        data: { posts },
+      } = response;
+      if (status === 200) {
+        setPosts(posts);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(posts);
+
   return (
     <div className="App">
-      <h1>Hello there!</h1>
+      <h1 className="text-5xl font-bold underline">Hello there!</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      "2xl": "1536px",
+    },
+    colors: {},
+    fontFamily: {
+      sans: [],
+      serif: [],
+    },
+    extend: {
+      spacing: {},
+      borderRadius: {},
+    },
+  },
+  plugins: [],
+};
