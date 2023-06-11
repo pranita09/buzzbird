9235d62dac27b0825c5eea7c9cbc1be8d22b7ddd
Pranita <pfulsundar8@gmail.com>
2023-06-12 02:10:43 +0530

    refactor: users data

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,13 +3,14 @@ import { Authentication } from "../pages/authentication/Authentication";
 import { Login } from "../pages/authentication/formComponents/Login";
 import { Signup } from "../pages/authentication/formComponents/Signup";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { Home } from "../pages/Home";
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
         <Route element={<PrivateRoutes />}>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
         </Route>
         <Route path="/auth" element={<Authentication />}>
           <Route path="login" element={<Login />} />
