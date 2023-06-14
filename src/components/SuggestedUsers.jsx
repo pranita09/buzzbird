421776a4fda7e664a4f12be15ac9b558c1720c46
Pranita <pfulsundar8@gmail.com>
2023-06-14 15:28:12 +0530

    refactor: refector jsx code

--- src/components/SuggestedUsers.jsx
+++ src/components/SuggestedUsers.jsx
@@ -20,18 +20,19 @@ const SuggestedUsers = () => {
         !userData.following.find((item) => item.username === eachUser.username)
     );
 
-  console.log(filteredUsers);
   return (
     <>
       {filteredUsers.length ? (
         <div className="flex flex-col justify-center gap-4 m-4 mt-0 px-4 py-3 bg-lighterPrimary rounded-md h-max w-full sticky top-[85px] z-0 dark:text-darkColor">
           <div className="text-lg font-bold tracking-wide">Who to Follow</div>
+
           {filteredUsers?.map((user) => (
             <div
               key={user._id}
               className="flex items-start gap-2 cursor-pointer"
             >
               <UserAvatar user={user} className="h-9 w-9" />
+
               <div className="flex flex-col grow -mt-0.5">
                 <span className="text-sm">
                   {user.firstName + " " + user.lastName}
@@ -40,6 +41,7 @@ const SuggestedUsers = () => {
                   @{user.username}
                 </span>
               </div>
+
               <PrimaryButton className="py-1 px-2 rounded-md">
                 Follow
               </PrimaryButton>
