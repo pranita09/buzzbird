bd39609370cd199a3e832d2b16023ba897761e17
Pranita <pfulsundar8@gmail.com>
2023-06-25 02:10:20 +0530

    fix: position UsersModal component

--- src/components/UsersModal/UsersModal.jsx
+++ src/components/UsersModal/UsersModal.jsx
@@ -7,11 +7,25 @@ const UsersModal = ({ usersListModal, setUsersListModal }) => {
 
   const navigate = useNavigate();
 
+  const styles = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "background.paper",
+    border: "1px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
-    <div className="bg-lighterPrimary text-sm border border-darkGrey p-4 w-80 rounded overflow-y-auto mx-4">
+    <div
+      style={styles}
+      className="bg-lighterPrimary text-sm border border-darkGrey p-4 w-[17rem] rounded-md overflow-y-auto mx-4"
+    >
       <div className="flex justify-between items-center mb-4">
-        <div className="text-xl">{title}</div>
-        <button className="hover:bg-darkGrey h-min hover:rounded-full px-2 py-0.5">
+        <div className="text-xl font-semibold">{title}</div>
+        <button className="hover:bg-darkGrey hover:text-lightGrey h-min w-min hover:rounded-full p-1">
           <FaTimes />
         </button>
       </div>
@@ -22,7 +36,10 @@ const UsersModal = ({ usersListModal, setUsersListModal }) => {
             <div
               key={user?._id}
               className="flex gap-2 cursor-pointer"
-              onClick={() => navigate(`/profile/${user?.username}`)}
+              onClick={() => {
+                navigate(`/profile/${user?.username}`);
+                setUsersListModal({ show: false, title: "", list: [] });
+              }}
             >
               <UserAvatar user={user} className="h-9 w-9" />
               <div className="flex flex-col -mt-0.5">
