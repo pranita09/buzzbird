084bba97a73e7e1edc500a132c7dff5e42416f43
Pranita <pfulsundar8@gmail.com>
2023-06-27 17:26:50 +0530

    feat: infinite scroll

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,14 +1,62 @@
 import { usePosts } from "../../contexts/post-context";
 import { SideBar, SearchBar, PostCard, SuggestedUsers } from "../../components";
 import { sortPosts } from "../../utils/sortPosts";
+import { actionTypes } from "../../utils/constants";
+import React, { useEffect, useRef } from "react";
 
 const Explore = () => {
   const {
-    postsState: { posts },
+    postsState: { posts, page, isNextPostLoading },
     isLoading,
+    postsDispatch,
   } = usePosts();
 
-  const sortedPosts = sortPosts(posts, "Latest");
+  const elementRef = useRef(null);
+
+  const { SET_PAGE, SET_IS_NEXT_POST_LOADING } = actionTypes;
+
+  const sortedPosts = sortPosts(posts, "Latest").slice(0, page * 5);
+
+  const handleObserver = (entries) => {
+    console.log("here");
+    const entry = entries[0];
+    console.log("INSIDE handleobserver", entry);
+    if (entry.isIntersecting) {
+      postsDispatch({ type: SET_IS_NEXT_POST_LOADING, payload: true });
+      postsDispatch({ type: SET_PAGE, payload: page + 1 });
+    }
+  };
+
+  console.log(page);
+  console.log(elementRef.current);
+
+  useEffect(() => {
+    if (sortedPosts.length > 0) {
+      const observer = new IntersectionObserver(handleObserver);
+      if (observer && elementRef.current) {
+        observer.observe(elementRef.current);
+      }
+      return () => {
+        if (observer) {
+          observer.unobserve(elementRef);
+        }
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  useEffect(() => {
+    let timeoutId;
+    if (isNextPostLoading) {
+      timeoutId = setTimeout(() => {
+        postsDispatch({ type: SET_IS_NEXT_POST_LOADING, payload: false });
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timeoutId);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isNextPostLoading, sortedPosts]);
 
   return (
     <div className="grid sm:grid-cols-[5rem_1fr] lg:grid-cols-[12rem_1fr] xl:grid-cols-[13rem_1fr_20rem] w-[100%] lg:w-[80%] mb-16 sm:m-auto dark:bg-darkGrey dark:text-lightGrey transition-all duration-500">
@@ -27,7 +75,19 @@ const Explore = () => {
             {isLoading ? (
               "Loader"
             ) : sortedPosts?.length > 0 ? (
-              sortedPosts.map((post) => <PostCard key={post._id} post={post} />)
+              <>
+                {sortedPosts.map((post, index) => {
+                  return (
+                    <React.Fragment key={post._id}>
+                      <PostCard post={post} />
+                      {index === sortedPosts?.length - 1 && (
+                        <div ref={elementRef} className="h-0" />
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+                {isNextPostLoading && <div>Loader more items</div>}
+              </>
             ) : (
               <div className="p-4 text-center text-lg font-bold">
                 No posts yet.
