15733075ecbb236e194303507a4ffee6e1f6de55
Pranita <pfulsundar8@gmail.com>
2023-06-30 15:19:12 +0530

    fix: dark theme

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -6,7 +6,7 @@ const PageNotFound = () => {
   const navigate = useNavigate();
   document.title = "Page Not Found";
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-10 -mt-10">
+    <div className="h-[110vh] flex flex-col justify-center items-center gap-10 -mt-10 dark:text-lightGrey">
       <img
         src={errorImage}
         alt="404Image"
