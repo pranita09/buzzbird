0a19e683ab6fc77989e4baf4fcfbdc1bc12abfb2
Pranita <pfulsundar8@gmail.com>
2023-07-03 23:32:27 +0530

    refactor: share post function

--- src/utils/sharePost.js
+++ src/utils/sharePost.js
@@ -1,8 +1,14 @@
 import { toast } from "react-hot-toast";
 
-export const sharePost = (postId) => {
-  navigator.clipboard.writeText(
-    `https://buzzbird-v1.netlify.app/post/${postId}`
-  );
-  toast.success("Link copied to clipboard.");
+export const sharePost = async (postId) => {
+  try {
+    await navigator.share({
+      title: "BuzzBird",
+      text: "Check out this post",
+      url: `https://buzzbird-v1.netlify.app/post/${postId}`,
+    });
+  } catch (error) {
+    console.error("Error while sharing: ", error);
+    toast.error("Something went wrong!");
+  }
 };
