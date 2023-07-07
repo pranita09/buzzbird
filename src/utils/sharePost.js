9dc89141ad06c89ca2a07c998e3a915b38413621
Pranita <pfulsundar8@gmail.com>
2023-07-07 13:03:09 +0530

    fix: sharePost functionality

--- src/utils/sharePost.js
+++ src/utils/sharePost.js
@@ -1,14 +1,23 @@
 import { toast } from "react-hot-toast";
 
+const postData = (id) => ({
+  title: "BuzzBird",
+  text: "Check out this post",
+  url: `https://buzzbird-v1.netlify.app/post/${id}`,
+});
+
 export const sharePost = async (postId) => {
-  try {
-    await navigator.share({
-      title: "BuzzBird",
-      text: "Check out this post",
-      url: `https://buzzbird-v1.netlify.app/post/${postId}`,
-    });
-  } catch (error) {
-    console.error("Error while sharing: ", error);
-    toast.error("Something went wrong!");
+  if (navigator.share && navigator.canShare(postData(postId))) {
+    try {
+      await navigator.share(postData(postId));
+    } catch (error) {
+      console.error("Error while sharing: ", error);
+      toast.error("Something went wrong!");
+    }
+  } else {
+    navigator.clipboard.writeText(
+      `https://buzzbird-v1.netlify.app/post/${postId}`
+    );
+    toast.success("Link Copied. Start sharing!");
   }
 };
